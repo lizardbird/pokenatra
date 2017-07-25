fa94d0714ef5da8c7d4534759ad2db5a03bc37a6
Liz Jewell <eghjewell@gmail.com>
2017-07-25 15:05:17 -0400

    MVP working

---
+++ app.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+require_relative 'db/connection'
+
+require_relative 'models/pokemon'
+
+get '/pokemons' do
+  @pokemons = Pokemon.all
+  erb :"/pokemons/index"
+end
+
+get '/pokemons/new' do
+  erb :"/pokemons/new"
+end
+
+post '/pokemons' do
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+get '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"/pokemons/profile"
+end
+
+get '/pokemons/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :'/pokemons/edit'
+end
+
+put '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+delete '/pokemons/:id' do
+  @pokemons = Pokemon.find(params[:id])
+  @pokemons.destroy
+  redirect '/pokemons'
+end
